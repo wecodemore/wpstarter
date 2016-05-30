d345f0088746449e52feabc3f3947d0b6324f138
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2016-05-30 14:08:17 +0200

    Make use of new Paths class and custom steps config

--- wpstarter/src/Setup.php
+++ wpstarter/src/Setup.php
@@ -12,20 +12,12 @@ namespace WCM\WPStarter;
 
 use Composer\Script\Event;
 use Composer\Composer;
-use ArrayObject;
 use WCM\WPStarter\Setup\Stepper;
 use WCM\WPStarter\Setup\StepperInterface;
 use WCM\WPStarter\Setup\Config;
 use WCM\WPStarter\Setup\FileBuilder;
 use WCM\WPStarter\Setup\OverwriteHelper;
 use WCM\WPStarter\Setup\IO;
-use WCM\WPStarter\Setup\Steps\CheckPathStep;
-use WCM\WPStarter\Setup\Steps\DropinsStep;
-use WCM\WPStarter\Setup\Steps\EnvExampleStep;
-use WCM\WPStarter\Setup\Steps\GitignoreStep;
-use WCM\WPStarter\Setup\Steps\IndexStep;
-use WCM\WPStarter\Setup\Steps\MoveContentStep;
-use WCM\WPStarter\Setup\Steps\WPConfigStep;
 
 /**
  * @author  Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
@@ -36,6 +28,9 @@ class Setup
 {
     const WP_PACKAGE = 'johnpbloch/wordpress';
 
+    /**
+     * @var bool
+     */
     private static $isRoot = false;
 
     /**
@@ -55,15 +50,12 @@ class Setup
         $wpStarterConfig = isset($extra['wpstarter']) && is_array($extra['wpstarter'])
             ? $extra['wpstarter']
             : [];
-        $config = new Config(
-            array_merge(
-                $wpStarterConfig,
-                ['is-root' => self::$isRoot, 'wp-version' => $wpVersion]
-            )
-        );
+        $wpStarterConfig['is-root'] = self::$isRoot;
+        $wpStarterConfig['wp-version'] = $wpVersion;
+        $config = new Config($wpStarterConfig);
         $io = new IO($event->getIO(), $config['verbosity']);
 
-        $instance->install($composer, $config, $io, $extra);
+        $instance->install(new Paths($event), $config, $io);
     }
 
     /**
@@ -82,41 +74,54 @@ class Setup
     /**
      * Run WP Starter installation adding all the steps to Builder and launching steps processing.
      *
-     * @param \Composer\Composer          $composer
+     * @param \WCM\WPStarter\Paths        $paths
      * @param \WCM\WPStarter\Setup\Config $config
      * @param \WCM\WPStarter\Setup\IO     $io
-     * @param array                       $extra
      */
-    private function install(Composer $composer, Config $config, IO $io, array $extra)
+    private function install(Paths $paths, Config $config, IO $io)
     {
-        $paths = $this->paths($composer, $extra);
         $overwrite = new OverwriteHelper($config, $io, $paths);
         $stepper = new Stepper($io, $overwrite);
         if ($this->stepperAllowed($stepper, $config, $paths, $io)) {
             $builder = new FileBuilder(self::$isRoot);
-            $stepper
-                ->addStep(new CheckPathStep())
-                ->addStep(new WPConfigStep($io, $builder))
-                ->addStep(new IndexStep($builder))
-                ->addStep(new EnvExampleStep($io, $builder))
-                ->addStep(new DropinsStep($io, $overwrite))
-                ->addStep(new GitignoreStep($io, $builder))
-                ->addStep(new MoveContentStep($io))
-                ->run($paths);
+
+            $ns = 'WCM\\WPStarter\\Setup\\Steps\\';
+            $classes = array_merge([
+                'check-path'   => $ns.'CheckPathStep',
+                'wp-config'    => $ns.'WPConfigStep',
+                'inded'        => $ns.'IndexStep',
+                'env-example'  => $ns.'EnvExampleStep',
+                'dropins'      => $ns.'DropinsStep',
+                'gitignore'    => $ns.'GitignoreStep',
+                'move-content' => $ns.'MoveContentStep',
+            ], $config['custom-steps']);
+
+            array_walk(
+                $classes,
+                function ($stepClass) use ($stepper, $io, $builder, $overwrite, $ns) {
+                    $step = is_subclass_of($stepClass, $ns.'FileStepInterface', true)
+                        ? new $stepClass($io, $builder, $overwrite)
+                        : new $stepClass($io);
+
+                    $stepper->addStep($step);
+                }
+            );
+
+            $stepper->run($paths);
         }
     }
 
     /**
-     * @param  \WCM\WPStarter\Setup\StepperInterface $stepper
-     * @param  \WCM\WPStarter\Setup\Config           $config
-     * @param  \ArrayObject                          $paths
-     * @param  \WCM\WPStarter\Setup\IO               $io
+     * @param \WCM\WPStarter\Setup\StepperInterface $stepper
+     * @param \WCM\WPStarter\Setup\Config           $config
+     * @param \WCM\WPStarter\Paths                  $paths
+     * @param \WCM\WPStarter\Setup\IO               $io
      * @return bool
      */
     private function stepperAllowed(
         StepperInterface $stepper,
         Config $config,
-        ArrayObject $paths,
+        Paths $paths,
         IO $io
     ) {
         if (! $stepper->allowed($config, $paths)) {
@@ -131,40 +136,6 @@ class Setup
         }
 
         return true;
-    }
-
-    /**
-     * Build paths object.
-     *
-     * According to johnpbloch/wordpress-core-installer package, WP subdir is 'wordpress' by
-     * default, but can can be customized via 'extra.wordpress-content-dir' setting.
-     * 'extra.wordpress-content-dir', instead, can be used to customized wp-content folder, with
-     * proper automatic url generation for content.
-     *
-     * @param  \Composer\Composer $composer
-     * @param  array              $extra
-     * @return \ArrayObject
-     */
-    private function paths(Composer $composer, array $extra)
-    {
-        $cwd = getcwd();
-        $wpInstallDir = isset($extra['wordpress-install-dir'])
-            ? $extra['wordpress-install-dir']
-            : 'wordpress';
-        $wpFullDir = "{$cwd}/{$wpInstallDir}";
-        $wpSubdir = $this->subdir($cwd, $wpFullDir);
-        $wpContent = isset($extra['wordpress-content-dir'])
-            ? $this->subdir($cwd, $cwd.'/'.$extra['wordpress-content-dir'])
-            : 'wp-content';
-
-        return new ArrayObject($this->normalisePaths([
-            'root'       => $cwd,
-            'vendor'     => $this->subdir($cwd, $composer->getConfig()->get('vendor-dir')),
-            'wp'         => $wpSubdir,
-            'wp-parent'  => $this->subdir($cwd, dirname($wpFullDir)),
-            'wp-content' => $wpContent,
-            'starter'    => $this->subdir($cwd, dirname(__DIR__)),
-        ]), ArrayObject::STD_PROP_LIST);
     }
 
     /**
@@ -184,34 +155,5 @@ class Setup
         }
 
         return $version ? implode('.', array_slice(explode('.', $version), 0, 3)) : '';
-    }
-
-    /**
-     * Just ensures paths use same separator, to allow search/replace.
-     *
-     * @param  array $paths
-     * @return array
-     */
-    private function normalisePaths(array $paths)
-    {
-        array_walk($paths, function (&$path) {
-            is_string($path) and $path = str_replace('\\', '/', $path);
-        });
-
-        return $paths;
-    }
-
-    /**
-     * Strips a parent folder from child.
-     *
-     * @param  string $root
-     * @param  string $path
-     * @return string string
-     */
-    private function subdir($root, $path)
-    {
-        $paths = $this->normalisePaths([$root, $path]);
-
-        return trim(preg_replace('|^'.preg_quote($paths[0]).'|', '', $paths[1]), '\\/');
     }
 }
