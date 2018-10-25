d44a64a147a7c26277072146e5149b1061abbd56
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2018-10-25 16:54:21 +0200

    Fix a bug in Io where str_repeat is used with 0 or negative number

--- src/Util/Io.php
+++ src/Util/Io.php
@@ -83,20 +83,24 @@ class Io
 
         $lines = static::ensureLength(...$lines);
         $topLength = max(array_map('strlen', array_map('trim', array_map('strip_tags', $lines))));
-        $length = 56;
+        $length = max($topLength, 56);
         $leftSpace = (int)floor(($length - $topLength) / 2);
+        $leftSpace > 0 or $leftSpace = 1;
 
-        $whiteLine = $before . str_repeat(' ', 60) . $after;
+        $whiteLine = "{$before}  " . str_repeat(' ', $length) . "  {$after}";
         $block = ['', $whiteLine];
         foreach ($lines as $line) {
+            $line = trim($line);
             $len = strlen(strip_tags($line));
+            $rightSpace = ($length - $leftSpace) - $len;
+            $rightSpace > 0 or $rightSpace = 1;
             if ($len < $length) {
-                $rightSpace = (56 - $leftSpace) - $len;
                 $line = str_repeat(' ', $leftSpace) . trim($line) . str_repeat(' ', $rightSpace);
             }
             $block[] = "{$before}  {$line}  {$after}";
         }
         $block[] = $whiteLine;
+        $block[] = '';
 
         return $block;
     }
