863a649174d52b504b0fda2c98a2a0cd8cbc1e5c
Philipp Bammes <8144115+tyrann0us@users.noreply.github.com>
2022-09-15 09:41:32 +0200

    Fix typo in `wp-config.php`

--- templates/wp-config.php
+++ templates/wp-config.php
@@ -303,7 +303,7 @@ CLEAN_UP : {
 
 ###################################################################################################
 #  I've seen things you people wouldn't believe. Attack ships on fire off the shoulder of Orion.  #
-#                 I watched C-beams glitter in the dark near the Tannhauser gate.                 #
+#                 I watched C-beams glitter in the dark near the Tannhäuser Gate.                 #
 #            All those moments will be lost in time, like tears in rain. Time to die.             #
 ###################################################################################################
 
